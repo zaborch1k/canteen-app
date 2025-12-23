5261d6839c6d6aca871566331fcd0759ba242ff5
zaborch1k <ezaroch13@gmail.com>
2025-12-23 20:07:34 +0300

    style: app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,23 +1,23 @@
 package app
 
 import (
-    "canteen-app/internal/adapter/http"
+	"canteen-app/internal/adapter/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 type App struct {
-    router *gin.Engine;
+	router *gin.Engine
 }
 
 func New() (*App, error) {
-    router := http.NewRouter() 
+	router := http.NewRouter()
 
-    return &App {
-        router: router,
-    }, nil
+	return &App{
+		router: router,
+	}, nil
 }
 
 func (a App) Run(port string) error {
-    return a.router.Run(port)
+	return a.router.Run(port)
 }
