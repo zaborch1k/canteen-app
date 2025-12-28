104fd5bb632f1e6019c81249a9edfb52862067b6
zaborch1k <ezaroch13@gmail.com>
2025-12-28 11:56:08 +0300

    feat: add requireRole auth middleware

--- internal/adapter/http/api/auth_middleware.go
+++ internal/adapter/http/api/auth_middleware.go
@@ -29,7 +29,28 @@ func AuthMiddleware(tokenService usecase.TokenService) gin.HandlerFunc {
 		}
 
 		c.Set("userID", claims.UserID)
-		c.Set("role", claims.Role)
+		c.Set("userRole", claims.Role)
+		c.Next()
+	}
+}
+
+func RequireRole(roles ...string) gin.HandlerFunc {
+	allowed := make(map[string]struct{}, len(roles))
+	for _, r := range roles {
+		allowed[r] = struct{}{}
+	}
+
+	return func(c *gin.Context) {
+		roleVal, ok := c.Get("userRole")
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "no role in context"})
+		}
+
+		role, _ := roleVal.(string)
+		if _, ok := allowed[role]; ok {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "forbidden"})
+			return
+		}
 		c.Next()
 	}
 }
