5f0ce0f6e442b38c0b23d0dd77491acfee800c3e
zaborch1k <ezaroch13@gmail.com>
2026-01-20 12:14:31 +0300

    fix: validation max in register/login requests

--- internal/adapter/http/common/dto.go
+++ internal/adapter/http/common/dto.go
@@ -1,8 +1,8 @@
 package common
 
 type RegisterRequest struct {
-	Login    string `json:"login" binding:"required" validate:"required,min=2,max=50" example:"the_real_slim_shady"`
-	Password string `json:"password" binding:"required" validate:"required,min=8,max=100" example:"password1234"`
+	Login    string `json:"login" binding:"required" validate:"required,max=50,min=2" example:"the_real_slim_shady"`
+	Password string `json:"password" binding:"required" validate:"required,max=100,min=8" example:"password1234"`
 	Name     string `json:"name" binding:"required" validate:"required,max=100,alpha" example:"Slim"`
 	Surname  string `json:"surname" binding:"required" validate:"required,max=100,alpha" example:"Shady"`
 	Role     string `json:"role" binding:"required" validate:"required" example:"admin"`
