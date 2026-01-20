4b25bb29dff67edcab414284145c3ea94a4089fe
zaborch1k <ezaroch13@gmail.com>
2026-01-20 13:18:31 +0300

    feat: add oneof validation for role (admin/employeee/user)

--- internal/adapter/http/common/dto.go
+++ internal/adapter/http/common/dto.go
@@ -5,7 +5,7 @@ type RegisterRequest struct {
 	Password string `json:"password" binding:"required" validate:"required,max=100,min=8" example:"password1234"`
 	Name     string `json:"name" binding:"required" validate:"required,max=100,alpha" example:"Slim"`
 	Surname  string `json:"surname" binding:"required" validate:"required,max=100,alpha" example:"Shady"`
-	Role     string `json:"role" binding:"required" validate:"required" example:"admin"`
+	Role     string `json:"role" binding:"required" validate:"required,oneof=admin employee user" example:"admin"`
 }
 
 type LoginRequest struct {
